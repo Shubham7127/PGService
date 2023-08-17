6d481f65bb86f484d86037d8bb79b44561dbe55e
Shubham7127 <phadtareshubham712@gmail.com>
2023-08-17 12:28:51 +0530

    pojos2

---
+++ PGService2/src/main/java/com/app/dao/PropertDao.java
@@ -0,0 +1,12 @@
+package com.app.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.pojos.Properties;
+
+public interface PropertDao extends JpaRepository<Properties, Long> {
+
+	public List<Properties> findByName(String city);
+}
